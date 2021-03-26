43a5e57dc1db06ce1913c7d5aebd1fa46f82412e
Celina Junker <junkerc2@nku.edu>
2021-03-26 15:22:22 -0400

    Creating vendor update, currently hard coded

---
+++ js/vendor-update.js
@@ -0,0 +1,57 @@
+//let id = $("#vid").val();
+//should be onclick()
+
+
+$().ready(()=>{
+    console.log("jQuery is ready!");
+    let id = $("#pid").val();
+    console.log("id = " + id);
+        vendorDetail(id)
+        .done((vendor) => {
+            display(vendor);
+            console.log(vendor);
+        })
+        .fail((err) => {
+            alert("Vendor not found!");
+        });
+    
+
+
+    $("#update").click(() => {
+
+        let vendor = {
+            id: $("#pid").val(),
+            code: $("#pcode").val(),
+            name: $("#pname").val(),
+            address: $("#paddress").val(),
+            city: $("#pcity").val(),
+            state: $("#pstate").val(),
+            zip: $("#pzip").val(),
+            phoneNumber: $("#pphone").val(),
+            email: $("#pemail").val()
+        }
+        console.log(vendor);
+        console.debug("vendor before:", vendor);
+        vendorCreate(vendor)
+            .done((res)=> {
+                console.log("Update successful:",res);
+            })
+            .fail((err) => {
+                console.error("update failed:",err);
+            });
+    });
+});
+
+const display = (vendor) => {
+    $("#pid").val(vendor.id);
+    $("#pcode").val(vendor.code);
+    $("#pname").val(vendor.name);
+    $("#paddress").val(vendor.address);
+    $("#pcity").val(vendor.city);
+    $("#pstate").val(vendor.state);
+    $("#pnumber").val(vendor.number);
+    $("#pzip").val(vendor.zip);
+    $("#pphone").val(vendor.phoneNumber ?? "NA");
+    $("#pemail").val(vendor.email ?? "NA");
+
+    }
